7275e711da7ceb7cb157534abf77e1271ee56eea
MacMini A6 <macminia6@MacMiniiiiiiiii.fpt>
2025-12-12 18:00:52 +0700

    update

--- SwiftfulThinkingContinuedLearning/LongPressGesture.swift
+++ SwiftfulThinkingContinuedLearning/LongPressGesture.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct LongPressGesture: View {
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { (isPressing) in
+                        //start press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        // at the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    }
+                
+                Text("RESET")
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            isComplete = false
+                            isSuccess = false
+                        }
+                    }
+            }
+            
+        }
+        
+//        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+////            .onTapGesture {
+////                isComplete.toggle()
+////            }
+//            .onLongPressGesture(minimumDuration: 1.0) {
+//                isComplete.toggle()
+//            }
     }
 }
 
