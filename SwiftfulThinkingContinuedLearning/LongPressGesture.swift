97366d6374ef49de4a6c626ba8f854cc44a55c79
MacMini A6 <macminia6@MacMiniiiiiiiii.fpt>
2025-12-12 17:35:55 +0700

    first

---
+++ SwiftfulThinkingContinuedLearning/LongPressGesture.swift
@@ -0,0 +1,18 @@
+//
+//  LongPressGesture.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by MacMini A6 on 12/12/25.
+//
+
+import SwiftUI
+
+struct LongPressGesture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LongPressGesture()
+}
---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by MacMini A6 on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
