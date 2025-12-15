bd75935404af0dda910b11128e5a487f2973586c
MacMini A6 <macminia6@MacMiniiiiiiiii.fpt>
2025-12-15 16:29:00 +0700

    Mask

---
+++ SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -0,0 +1,52 @@
+//
+//  MaskBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by MacMini A6 on 15/12/25.
+//
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    @State var rating: Int = 0
+    
+    var body: some View {
+        ZStack {
+            starsView
+                .overlay {
+                    overlayView
+                        .mask(starsView)
+                }
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundStyle(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    MaskBootcamp()
+}
