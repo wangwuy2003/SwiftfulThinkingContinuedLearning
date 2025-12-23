bc41153d9d3ec559820add90bb8c859537852e87
MacMini A6 <macminia6@MacMiniiiiiiiii.fpt>
2025-12-23 17:19:18 +0700

    CoreData with MVVM

---
+++ SwiftfulThinkingContinuedLearning/CoreDataBootcamp.swift
@@ -0,0 +1,123 @@
+//
+//  CoreDataBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by MacMini A6 on 23/12/25.
+//
+
+import SwiftUI
+import CoreData
+import Combine
+
+class CoreDataViewModel: ObservableObject {
+    let container: NSPersistentContainer
+    
+    @Published var savedEntities: [FruitEntity] = []
+    
+    init() {
+        container = NSPersistentContainer(name: "FruitsContainer")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("ERROR LOADING CORE DATA. \(error)")
+            } else {
+                print("Successfully loaded core data!")
+            }
+        }
+        
+        fetchFruits()
+    }
+    
+    func fetchFruits() {
+        let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+        
+        do {
+            savedEntities = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching: \(error)")
+        }
+    }
+    
+    func addFruit(text: String) {
+        let newFruit = FruitEntity(context: container.viewContext)
+        newFruit.name = text
+        saveData()
+    }
+    
+    func deleteFruit(indexSet: IndexSet) {
+        guard let index = indexSet.first else {
+            return
+        }
+        let entity = savedEntities[index]
+        container.viewContext.delete(entity)
+        saveData()
+    }
+    
+    func updateFruit(entity: FruitEntity) {
+        let currentName = entity.name ?? ""
+        let newName = currentName + "!"
+        entity.name = newName
+        saveData()
+    }
+    
+    func saveData() {
+        do {
+            try container.viewContext.save()
+            fetchFruits()
+        } catch let error {
+            print("Error saving: \(error)")
+        }
+    }
+}
+
+struct CoreDataBootcamp: View {
+    @StateObject var vm = CoreDataViewModel()
+    
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Enter fruit here...", text: $textFieldText)
+                    .font(.headline)
+                    .padding(.leading)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal)
+                
+                Button {
+                    guard !textFieldText.isEmpty else {
+                        return
+                    }
+                    vm.addFruit(text: textFieldText)
+                    textFieldText = ""
+                } label: {
+                    Text("Add Fruit")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .padding(.horizontal)
+                
+                List {
+                    ForEach(vm.savedEntities) { entity in
+                        Text(entity.name ?? "NO NAME")
+                            .onTapGesture {
+                                vm.updateFruit(entity: entity)
+                            }
+                    }
+                    .onDelete(perform: vm.deleteFruit)
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("Fruits")
+        }
+    }
+}
+
+#Preview {
+    CoreDataBootcamp()
+}
